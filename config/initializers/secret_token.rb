ac86894506e5a82f503c31932c56582ae085e50e
Patty O'Callaghan <patty19@gmail.com>
2013-09-05 06:21:04 -0430

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ItineriumV1::Application.config.secret_key_base = '993f6d63269208f45a152f3610f82dff780c21d0172152aa01fa3169039cfbcc0e4aa3260180e05655248785d09b61cbe61f8302e56c681f1d7b7e09080d2cec'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ItineriumV1::Application.config.session_store :cookie_store, key: '_itinerium_v1_session'
